1bd4931cc70df72a55a533a8b54adfc65e28193e
Artur <info@aigroupusa.com>
2021-01-20 21:39:34 -0500

    commit tree 37e1f30acf5a817ac9a88dfb4d222b1429563b5d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(x)
+  enu = (2..(x-1)).to_a
+  
+    if x == 1
+  return false
+    elsif x <= 0
+  return false
+    elsif enu.find { |el|  x % el == 0 }.nil? == true
+  return true
+    else
+  return false
+    end
+
+end
+
+
